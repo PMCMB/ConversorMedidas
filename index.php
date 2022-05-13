fe2a48a591b5a0c5530325e26db45a63ce2a856e
pmelo <7341868@formacao.iefp.pt>
2022-05-13 17:19:51 +0100

    Incluido um seletor para graus celsius ou fahrenheit.

--- index.php
+++ index.php
@@ -1,34 +1,102 @@
-
 <?php
+
+//Celsius to Fahrenheit ° F = 9/5 ( ° C) + 32
+//Fahrenheit to Celsius ° C = 5/9 (° F - 32)
+
+$valor_temp=0.0;
 $mostrar = false;
-$tmpC = 0.0;
-if( isset( $_POST["tempF"] ) ) {
-    $tmpF = floatval($_POST["tempF"]);
-    $tmpC = ($tmpF - 32) * (5 / 9);
-    $mostrar = true;
+
+//Fahrenheit to celsius
+function fahrenheit_to_celsius($given_value)
+{
+    $celsius=5/9*($given_value-32);
+    return $celsius ;
+}
+//Celsius to fahrenheit
+function celsius_to_fahrenheit($given_value)
+{
+    $fahrenheit=$given_value*9/5+32;
+    return $fahrenheit ;
 }
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
+
+    <style>
+        body {
+            background-color: #E6E6FA;
+        }
+    </style>
+
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Conversor de temperaturas</title>
-    <h1 style="text-align:center;color:white;background-color:DodgerBlue; border:2px solid gray;">Conversor de Medidas</h1>
 
+    <title>Conversor de Temperatura</title>
+
+    <h1 style="text-align:center;color:white;background-color:DodgerBlue;
+        border:2px solid gray;">Conversor de Temperatura</h1>
 </head>
 <body>
-<form></form>
-<form method="post">
-    <label for="tempF" >Temperatura em Farhneith</label>
-    <input type="number" name="tempF" required>
-    <input type="submit" value="Converter">
+
+<form action="" method="post">
+    <table>
+        <tr>
+            <td>
+                <input type="text" name="valor_temp">
+            </td>
+        </tr>
+        <tr>
+            <td> <br>
+                <select name="tipo_temp_nome">
+                    <option value="fahrenheit">Fahrenheit</option>
+                    <option value="celsius">Celsius</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td> <br>
+                <input type="submit" name="btn" value="Converter">
+            </td>
+        </tr>
+        <tr>
+            <td><br><hr/>
+                <h3>
+                    <?php
+
+                    if(isset($_POST['btn']))
+                    {
+                        $tipo_temp_nome=$_POST['tipo_temp_nome'];
+                        $valor_temp=$_POST['valor_temp'];
+
+                        if (empty($valor_temp)) {
+                            echo 'Atenção!!! Campo vazio, tem de preencher com um número!';
+                        }else{
+                            $mostrar = true;
+                        }
+                        if($mostrar) {
+                            //Fahrenheit to celsius
+                            if ($tipo_temp_nome=='fahrenheit') {
+                                $celsius=fahrenheit_to_celsius($valor_temp);
+                                echo "$valor_temp Fahrenheit = $celsius Celsius";
+                            }
+                            //Kelvin to fahrenheit
+                            else{
+                                $fahrenheit=celsius_to_fahrenheit($valor_temp);
+                                echo "$valor_temp Celsius  = $fahrenheit Fahrenheit";
+                            }
+                        }
+                    }
+                    ?>
+                </h3>
+                <hr/>
+            </td>
+        </tr>
+    </table>
+
 </form>
-<?php if($mostrar) {?>
-    <h1>Resultado da conversão:</h1>
-    <p><?php echo $tmpC ?> ºC</p>
-<?php } ?>
 </body>
 </html>
